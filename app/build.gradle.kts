cbf360b9521cdaaeffc0d0f893b0cbbd9faa977a
fnotfoundexception <nininanaquoi@gmail.com>
2025-03-06 15:47:55 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,8 +54,6 @@ dependencies {
     implementation ("com.squareup.picasso:picasso:2.8")
 
     //Room
-
-    //Room
     implementation ("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     ksp ("androidx.room:room-compiler:2.6.1")
